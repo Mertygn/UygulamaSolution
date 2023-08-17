21a22415040c5a14b641291cbe17ced88143a130
Mert Aygün <Mert Aygn@MERT>
2023-08-17 15:01:30 +0300

    Add project files.

---
+++ FirstExample/Program.cs
@@ -0,0 +1,40 @@
+﻿// İki sayı toplamı
+//int sayi1 = 3;
+//int toplam = sayi1 + sayi2;
+//Console.WriteLine(toplam);
+//Console.WriteLine("İki sayının toplamı:"+ sayi1+"+"+sayi2+"="+toplam);
+
+
+// kod satırları + - / * işlemleri
+//double a = 8;
+//double b = 6;
+
+//double toplam = a + b;
+//double fark = a - b;
+//double bolme = a / b;
+//double carpım = a * b;
+
+//Console.WriteLine("Sayıların Toplamı =" + toplam);
+//Console.WriteLine("Sayıların Farkı =" + fark);
+//Console.WriteLine("Sayıların Bölmesi =" + bolme);
+//Console.WriteLine("Sayıların Çarpımı =" + carpım);
+
+//---------------------------------------------------------------------------------------
+
+double kg = 75;
+double cm = 1.78;
+
+double snc = kg / (cm * cm);
+//Console.WriteLine("Vücüt Kitle İndeksiniz: " + snc);
+//----------------------------------------------------------------------------------------
+// daire cevre alan hesaplama
+
+double r = 5;
+double pi = 3.14;
+
+double alan = pi * (r * r);
+Console.WriteLine("Dairenin alanı:"+alan);
+double cevre = 2 * pi * r;
+Console.WriteLine("dairenin Çevresi:"+cevre);
+
+
